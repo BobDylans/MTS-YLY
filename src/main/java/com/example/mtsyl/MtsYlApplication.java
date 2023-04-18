648d111f679a6fbd38bff6c812aec155d15ead05
BobDylans <119731046+BobDylans@users.noreply.github.com>
2023-04-18 15:30:18 +0800

    Initial commit

---
+++ src/main/java/com/example/mtsyl/MtsYlApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtsyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsYlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtsYlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mtsyl/MtsYlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mtsyl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsYlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
